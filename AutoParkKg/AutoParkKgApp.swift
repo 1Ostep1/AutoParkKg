15e779171502988c52deea8d782f20d8773e4c7b
Ramazan <ramazan@mustard.love>
2022-12-13 22:45:09 +0600

    Initial Commit

---
+++ AutoParkKg/AutoParkKgApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoParkKgApp.swift
+//  AutoParkKg
+//
+//  Created by Iusupov Ramazan on 13/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AutoParkKgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
